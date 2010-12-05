0818a28551f4081f9a2a50cef9110b2297a21f0e
emlynoregan@gmail.com <emlynoregan@gmail.com@8cd67a55-0add-95e4-24da-82fa762e194d>
2010-12-05 01:36:18 +0000

    Trivial change

--- stepping-stone/src/ui/NoAuthTestHandler.py
+++ stepping-stone/src/ui/NoAuthTestHandler.py
@@ -6,7 +6,7 @@ class NoAuthTestHandler(webapp.RequestHandler):
         user = users.get_current_user()
         
         if user:
-            self.response.out.write("<p>Auth isn't required, but you are authed as %s.</p>" % user.email())
+            self.response.out.write("<p>Auth isn't required, but you are authed as %s.h, tha</p>" % user.email())
         else:
             self.response.out.write("<p>Auth isn't required, and you are not authed.</p>")
             
